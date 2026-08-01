5b95558e903070ca85b33c14cf578b03c4e9358a
William Pickard <williamhmpickard@gmail.com>
2026-08-01 10:55:31 +0200

    Add iOS app target

---
+++ RunTogether/RunTogether/RunTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunTogetherApp.swift
+//  RunTogether
+//
+//  Created by William Pickard on 2026/08/01.
+//
+
+import SwiftUI
+
+@main
+struct RunTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
