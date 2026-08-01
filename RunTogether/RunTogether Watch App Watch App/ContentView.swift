87e18f82e448d1bf5963f58f9614d8536f75ecdb
William Pickard <williamhmpickard@gmail.com>
2026-08-01 12:02:51 +0200

    Watch App

---
+++ RunTogether/RunTogether Watch App Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunTogether Watch App Watch App
+//
+//  Created by William Pickard on 2026/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunTogether/RunTogether Watch App Watch App/RunTogether_Watch_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunTogether_Watch_AppApp.swift
+//  RunTogether Watch App Watch App
+//
+//  Created by William Pickard on 2026/08/01.
+//
+
+import SwiftUI
+
+@main
+struct RunTogether_Watch_App_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
